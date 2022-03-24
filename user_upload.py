504cefce9243fb80f7e6f7f1e4ccf25e7f51b084
Wanderer <wanderer.a@gmail.com>
2022-03-24 21:41:10 +0300

    add gitignore

--- user_upload.py
+++ user_upload.py
@@ -1,7 +1,25 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import sys
+import resources.messages as m
+
+
+def print_help():
+    print(m.help_message)
+
+
+def create_table():
+    print(f'Create Table')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    arg = sys.argv
+    try:
+        arg[1:].index('--help')
+        print_help()
+        sys.exit()
+    except ValueError:
+        pass
+    try:
+        arg[1:].index('--create_table')
+        create_table()
+    except ValueError:
+        pass
